ee15ccd4fd36169b8a60aca1196d15ca39bc89b7
Teddy Bradsher <teddybradsher@gmail.com>
2020-07-20 13:52:03 -0400

    project complete

--- author.rb
+++ author.rb
@@ -1,1 +1,50 @@
 
+class Author
+  attr_reader :name
+  @@all = []
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+
+  def self.all
+    @@all
+  end
+
+
+  def books
+    Book.all.select do|book|
+        book.author == self
+    end
+  end
+
+
+  def write_book (title, word_count)
+    Book.new(self, title, word_count)
+  end
+
+  def total_words
+    wordcount = 0    
+    books.map do |book|
+    wordcount += book.word_count
+    end
+    wordcount
+  end
+  def self.most_words
+    most_words = 0
+    highest = ""
+    self.all.each do |most|
+        words = most.total_words
+        if words > most_words
+            most_words = words
+            highest = most.name
+    end
+end
+    highest
+  end
+
+ 
+"hi"
+
+end
--- console.rb
+++ console.rb
@@ -2,6 +2,11 @@ require 'pry'
 require_relative './book'
 require_relative './author'
 
+
+teddy_bradsher = Author.new("Teddy bradsher")
+teddy = Author.new("teddy bradsher")
+book = Book.new(teddy, "Ted", 345)
+bookzzzzz = Book.new(teddy, "Ted", 346)
 binding.pry
 
-
+"whatever"
